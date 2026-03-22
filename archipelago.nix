6b4978ae29bc26bcb6de16bedd6dc778f4c53d8d
Sten <sten@stenjamin.com>
2026-03-22 23:28:58 +1100

    first commit

---
+++ archipelago.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ...}
+
+{
+
+  # USB2SNES udev rule
+  services.udev.extraRules = ''
+    SUBSYSTEM=="tty", ATTRS{idVendor}=="1209", ATTRS{idProduct}=="5a22", MODE="0666"
+  '';
+
+  environment.systemPackages = with pkgs; [
+  pkgs.archipelago
+  pkgs.qusb2snes
+  pkgs.poptracker
+  ];
+
+
+}
---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+
+  description = "Sten's System Config";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ...} :
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations = {
+        sten-nix = lib.nixosSystem {
+          inherit system;
+	  modules = [
+	    ./configuration.nix
+	  ];
+	};
+      };
+    };
+
+}
