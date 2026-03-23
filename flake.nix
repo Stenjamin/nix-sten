4179a0e5c51aa1dadc88cf3e4916795536ec30c9
stenjamin <stephen.philp@hotmail.com>
2026-03-23 23:02:14 +1100

    Add home.nix, and home manager to flake

--- flake.nix
+++ flake.nix
@@ -4,12 +4,15 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ...} :
+  outputs = { self, nixpkgs, home-manager, ...} :
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
         sten-nix = lib.nixosSystem {
@@ -19,6 +22,12 @@
 	    ./archipelago.nix
 	  ];
 	};
+      };
+      homeConfigurations = {
+	stephen = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+	  modules = [ ./home.nix ];
+        };
       };
     };
 
