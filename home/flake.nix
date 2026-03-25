b7ccaf37ff2ded28f602fa1d2bee86807bf4627f
stenjamin <stephen.philp@hotmail.com>
2026-03-25 21:47:08 +1100

    Add plasma-manager to flake.nix

--- home/flake.nix
+++ home/flake.nix
@@ -8,21 +8,32 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
   };
 
-  outputs = {nixpkgs, home-manager, ... }:
+  outputs = {nixpkgs, home-manager, plasma-manager, ... }:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system; 
-      };
+      pkgs = import nixpkgs { inherit system; };
+      username = "stephen";
     in {
       homeConfigurations = {
         stephen = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [
+            inputs.plasma-manager.homeModules.plasma-manager
             ./home.nix
+            {
+              home = {
+                inherit username;
+                homeDirectory = "/home/${username}";
+              };
+            }
           ];
         };
       };
